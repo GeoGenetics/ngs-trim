4667321217094703a1cb1743cfa7f1b640d63ab3
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-09-29 12:35:11 +0200

    test: Add unit tests (#9)
    Add unit testing.

---
+++ examples/HD827sonic/.tests/unit/test_fastqc_raw.py
@@ -0,0 +1,59 @@
+"""
+Rule test code for unit testing of rules generated with Snakemake 9.11.2.
+"""
+
+
+import os
+import sys
+import shutil
+import tempfile
+from pathlib import Path
+from subprocess import check_output
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def test_fastqc_raw(conda_prefix):
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        config_path = Path(".tests/unit/fastqc_raw/config")
+        data_path = Path(".tests/unit/fastqc_raw/data")
+        expected_path = Path(".tests/unit/fastqc_raw/expected")
+
+        # Copy config to the temporary workdir.
+        shutil.copytree(config_path, workdir)
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir, dirs_exist_ok=True)
+
+        # Run the test job.
+        check_output(
+            [
+                "python",
+                "-m",
+                "snakemake",
+                "stats/reads/fastqc/raw/HD827sonic_2_lib1_L001_R2.html",
+                "stats/reads/fastqc/raw/HD827sonic_2_lib1_L001_R2_fastqc.zip",
+                "--snakefile",
+                "../../workflow/Snakefile",
+                "-f",
+                "--notemp",
+                "-j1",
+                "--target-files-omit-workdir-adjustment",
+                "--configfile",
+                "config/config.yaml",
+                "--software-deployment-method",
+                "conda",
+                "--directory",
+                workdir,
+            ]
+            + conda_prefix
+        )
+
+        # Check the output byte by byte using cmp/zmp/bzcmp/xzcmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        import common
+        common.OutputChecker(data_path, expected_path, workdir).check()
