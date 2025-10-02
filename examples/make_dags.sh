c1dd2671598f51b11f2e24a7c61fa865ba5328a9
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-10-02 10:19:57 +0200

    ci: Add unit testing to CI (#10)

--- examples/make_dags.sh
+++ examples/make_dags.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SNAKEMAKE_OPTS="--snakefile ../../workflow/Snakefile --configfile config/config.yaml --keep-storage-local-copies --forceall $@"
+set -euxo pipefail
+
+SNAKEMAKE_OPTS="--snakefile ../../workflow/Snakefile --configfile config/config.yaml --software-deployment-method conda --keep-storage-local-copies --forceall $@"
 
 for TEST in HD827sonic
 do
@@ -9,5 +11,9 @@ do
     snakemake $SNAKEMAKE_OPTS --rulegraph | dot -Tsvg > rulegraph.svg
     snakemake $SNAKEMAKE_OPTS --filegraph | dot -Tsvg > filegraph.svg
     snakemake $SNAKEMAKE_OPTS --dag | dot -Tsvg > dag.svg
+
+    #snakemake -j 10 $SNAKEMAKE_OPTS --notemp
+    #snakemake $SNAKEMAKE_OPTS --generate-unit-tests
+    pytest -p no:cacheprovider .tests/unit/
     cd ../
 done
