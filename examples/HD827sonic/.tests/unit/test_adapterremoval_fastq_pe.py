d60147bdcb3c45712242bedad9d10873ddb7ed7b
fgvieira <1151762+fgvieira@users.noreply.github.com>
2026-02-03 10:55:07 +0100

    Bump versions and small tweaks to unit tests

--- examples/HD827sonic/.tests/unit/test_adapterremoval_fastq_pe.py
+++ examples/HD827sonic/.tests/unit/test_adapterremoval_fastq_pe.py
@@ -41,6 +41,8 @@ def test_adapterremoval_fastq_pe(conda_prefix):
                 "--show-failed-logs",
                 "-j1",
                 "--target-files-omit-workdir-adjustment",
+                "--allowed-rules",
+                "adapterremoval_fastq_pe",
                 "--configfile",
                 "config/config.yaml",
                 "--software-deployment-method",
